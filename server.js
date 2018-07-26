ed9ed28180d5ad9fc3ccc9e7b2f85b2274524c8c
Dmytro Andrushko <andrushkodima@gmail.com>
2018-07-26 18:51:52 +0300

    2 commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var fs=require('fs');
+
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname+'/index.html')
